7375f932d9d4b74be530dfbf3b594eed9ef86fc6
Artem Golub <artem.golub2019@gmail.com>
2024-10-05 01:30:39 +0300

    Add convert from minutes to seconds

--- DateTime.pas
+++ DateTime.pas
@@ -2,5 +2,8 @@
   var N := ReadInteger('Введите ваш год:');
   Assert(N > 0);
   var vis := (N.Divs(4) and not(N.Divs(100))) or N.Divs(400);
-  Print($'Ваш год високосный? {vis}');
+  Println($'Ваш год високосный? {vis}');
+  
+  Println();
+  Println($'Секунд в минуте: {1 * 60}');
 end.
