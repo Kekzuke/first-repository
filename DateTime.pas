c88c48bcd0e33d0d98bc84de408cb3867454cadd
Artem Golub <artem.golub2019@gmail.com>
2024-10-05 02:03:44 +0300

    Add sum days between year1 and year2

--- DateTime.pas
+++ DateTime.pas
@@ -11,6 +11,8 @@
   
   var (d1, m1) := ReadInteger2('Введите день и месяц №1:');
   var (d2, m2) := ReadInteger2('Введите день и месяц №2:');
+  Assert((d1 in (0..31)) and (d2 in (0..31)));
+  Assert((m1 in (0..12)) and (m2 in (0..12)));
   if (12 - m1) < (12 - m2) then
     Println(1)
   else
@@ -22,5 +24,17 @@
     else
       Println(2);
   end;
-  
+  var y1 := ReadInteger('Введите год №1');
+  var y2 := ReadInteger('Введите год №2');
+  var sum := 0;
+  Assert((y1 > 0) and (y2 > 0));
+  for var i := y1 to y2 do
+  begin
+    var vis2 := (i.Divs(4) and not (i.Divs(100))) or i.Divs(400);
+    if vis2 then
+      sum += 366
+    else
+      sum += 365;
+  end;
+  Println($'Количество дней между этими годами(Включительно): {sum}');
 end.
