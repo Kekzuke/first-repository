6171cd39496bc7447c4e0bb9f86c78a83568b555
Artem Golub <artem.golub2019@gmail.com>
2024-10-05 01:31:21 +0300

    Revert "Add convert from minutes to seconds"
    This reverts commit 7375f932d9d4b74be530dfbf3b594eed9ef86fc6.

--- DateTime.pas
+++ DateTime.pas
@@ -2,8 +2,5 @@
   var N := ReadInteger('Введите ваш год:');
   Assert(N > 0);
   var vis := (N.Divs(4) and not(N.Divs(100))) or N.Divs(400);
-  Println($'Ваш год високосный? {vis}');
-  
-  Println();
-  Println($'Секунд в минуте: {1 * 60}');
+  Print($'Ваш год високосный? {vis}');
 end.
